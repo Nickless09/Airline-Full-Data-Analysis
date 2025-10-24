d584dcb65f6ef777c6a87dc2ecf01e422be54c34
Sem <22419023+Nickless09@users.noreply.github.com>
2025-10-24 12:44:43 +0200

    Update appairline.py

--- appairline.py
+++ appairline.py
@@ -222,7 +222,7 @@ if price_to_use and not filtered_df.empty:
         x=source_col,
         y=dest_col,
         z=price_to_use,
-        color_continuous_scale=px.colors.sequential.Viridis,
+        color_continuous_scale=px.colors.sequential.Sunset,
         title=f"Heatmap of Average Flight Prices by Route ({currency})",
         labels=label_map
     )
