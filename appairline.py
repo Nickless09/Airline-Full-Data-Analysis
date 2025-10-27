12820f3d4ccb78dfcdea9e5aa5fe5910b3c7a69e
Sem <22419023+Nickless09@users.noreply.github.com>
2025-10-27 09:35:31 +0100

    Update appairline.py

--- appairline.py
+++ appairline.py
@@ -228,16 +228,7 @@ if price_to_use and days_left_col and not filtered_df.empty:
 #         st.plotly_chart(fig3, use_container_width=True)
 
 
-if price_to_use and duration_col and not filtered_df.empty:
-    with st.expander("Show Flight Duration vs Price Chart"):
-        st.subheader(f"Flight Duration vs. Price ({currency})")
-        fig3 = px.scatter(
-            filtered_df, x=duration_col, y=price_to_use,
-            color=class_col if class_col else None,
-            title=f"Flight Duration vs Price ({currency})" + (" (by Class)" if class_col else ""),
-            labels=label_map
-        )
-        st.plotly_chart(fig3, use_container_width=True)
+
 
 # --- Heatmap using px.imshow ---
 if price_to_use and not filtered_df.empty:
@@ -266,3 +257,16 @@ if price_to_use and not filtered_df.empty:
         st.plotly_chart(fig_heatmap, use_container_width=True)
     else:
         st.info("No route data available to display heatmap.")
+
+# --- Charts (continous) ---
+
+if price_to_use and duration_col and not filtered_df.empty:
+    with st.expander("Show Flight Duration vs Price Chart"):
+        st.subheader(f"Flight Duration vs. Price ({currency})")
+        fig3 = px.scatter(
+            filtered_df, x=duration_col, y=price_to_use,
+            color=class_col if class_col else None,
+            title=f"Flight Duration vs Price ({currency})" + (" (by Class)" if class_col else ""),
+            labels=label_map
+        )
+        st.plotly_chart(fig3, use_container_width=True)
